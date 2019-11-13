a6a61651ccde6462ad5bbbe3e8ac3967a30708ff
jiangjiwei <jiangjiwei0322@outlook.com>
2019-11-13 11:26:34 +0800

    在 MainActivity 增加演示实例

--- app/src/main/java/com/osapling/overscrollmodule/MainActivity.kt
+++ app/src/main/java/com/osapling/overscrollmodule/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.osapling.overscrollmodule
 
 import android.os.Bundle
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scrollView = findViewById<ScrollView>(R.id.sv_main_root)
+        OverScrollDecoratorHelper.setUpOverScroll(scrollView)
     }
 }
